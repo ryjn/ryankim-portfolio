bcc1c4a92bf56e437081a0c1250f626ca144327a
Ryan Kim <ryankim@Ryans-MacBook-Pro.local>
2025-03-21 00:47:07 -0400

    navbar and mobile menu setup

--- project-todo.md
+++ project-todo.md
@@ -1,8 +1,6 @@
 # Portfolio Project
-
 Create a portfolio using ReactJS and TailwindCSS
 
 ## 3/20/25
-
-- [ ] Setup project structure
-- [ ] 1hr coding
+- [x] Setup project structure
+- [x] 1hr coding
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
-export const Navbar = () => {
+import { useEffect } from "react";
+
+export const Navbar = ({ menuOpen, setMenuOpen }) => {
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? "hidden" : "";
+  }, [menuOpen]);
+
   return (
-    <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+    <nav
+      className={`fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg ${menuOpen ? "opacity-20" : "opacity-100"}`}
+    >
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-mono text-xl font-bold text-white">
             Ryan<span className="text-blue-500"> Kim</span>
           </a>
-          <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden">
+          <div
+            className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
+            onClick={() => setMenuOpen((prev) => !prev)}
+          >
             &#9776;
           </div>
           <div className="hidden md:flex items-center space-x-8">
